ec292d08575aa95563a732d310a15e78a57b9972
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-01-03 19:36:25 +0800

    chore: deploy token to mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,11 +9,16 @@ const config: HardhatUserConfig = {
     goerli: {
       url: "https://rpc.ankr.com/eth_goerli",
       accounts: [ String(process.env.DEPLOYER_PRIVATE_KEY) ]
+    },
+    mainnet: {
+      url: "https://rpc.ankr.com/eth",
+      accounts: [ String(process.env.DEPLOYER_PRIVATE_KEY) ]
     }
   },
   etherscan: {
     apiKey: {
-      goerli: String(process.env.ETHERSCAN_API_KEY)
+      goerli: String(process.env.ETHERSCAN_API_KEY),
+      mainnet: String(process.env.ETHERSCAN_API_KEY)
     }
   }
 };
