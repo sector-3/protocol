b9d8cbe1f11e41bed80b52a9718b51105f4f0f3e
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-01-03 14:39:08 +0800

    feat: add token

---
+++ contracts/token/SECTOR3.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract SECTOR3 is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("Sector#3", "SECTOR3") ERC20Permit("Sector#3") {
+        _mint(msg.sender, 2049 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
