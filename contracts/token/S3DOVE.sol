4262063c9c82cae938609230472fc251e6bfff98
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-03-14 16:07:37 +0800

    rename nft

--- contracts/token/S3DOVE.sol
+++ contracts/token/S3DOVE.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Sector3Dove is ERC721, ERC721Enumerable, Ownable {
+contract S3DOVE is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
