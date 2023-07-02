3cf35273690f81353cd49d07c046eec83749f162
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-07-02 16:04:03 +0800

    refactor: use capped token supply

--- contracts/token/SECTOR3.sol
+++ contracts/token/SECTOR3.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract SECTOR3 is ERC20, ERC20Permit, ERC20Votes {
-    constructor() ERC20("Sector#3", "SECTOR3") ERC20Permit("Sector#3") {
+contract SECTOR3 is ERC20Capped, ERC20Permit, ERC20Votes {
+    constructor() ERC20("Sector#3", "SECTOR3") ERC20Capped(2049 * 1e18) ERC20Permit("Sector#3") {
         _mint(msg.sender, 2049 * 1e18);
     }
 
@@ -21,7 +21,7 @@ contract SECTOR3 is ERC20, ERC20Permit, ERC20Votes {
 
     function _mint(address to, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        override(ERC20, ERC20Capped, ERC20Votes)
     {
         super._mint(to, amount);
     }
